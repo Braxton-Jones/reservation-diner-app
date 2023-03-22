db5f8f6841c3d6de74476d28a5be3094ab324862
Braxton <65457602+braxttsl-dev@users.noreply.github.com>
2023-03-22 17:06:16 -0400

    "changes: deployment fix"

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,9 @@ function App() {
 	const [currentReservation, setCurrentReservation] = useState([]);
 	useEffect(() => {
 		const fetchReservations = async () => {
-			const response = await fetch('http://localhost:4000/api/reservations/');
+			const response = await fetch(
+				'https://reservation-api-ujvu.onrender.com/',
+			);
 			const json = await response.json();
 			setReservations(json);
 		};
